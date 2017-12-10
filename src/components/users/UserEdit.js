45884b62fe2ebab45b8a1adf79c7818cc5029335
sarahalpay <sarah.g.alpay@gmail.com>
2017-12-10 16:42:26 +0000

    styling done to Trip Edit and Trip Add page, but these are not displaying anymore

--- src/components/users/UserEdit.js
+++ src/components/users/UserEdit.js
@@ -16,7 +16,6 @@ class UserEdit extends React.Component {
   };
 
   componentDidMount() {
-    console.log(this);
     Axios
       .get(`/api/users/${this.props.match.params.id}`)
       .then(res => this.setState({ user: res.data }))
