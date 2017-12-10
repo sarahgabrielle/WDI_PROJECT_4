b486ef50d7b4ae7a79225d058b37d829f8a8e079
sarahalpay <sarah.g.alpay@gmail.com>
2017-12-10 11:57:43 +0000

    video added to landing page and started styling on user Show page

--- src/components/users/UserShow.js
+++ src/components/users/UserShow.js
@@ -4,6 +4,7 @@ import Axios from 'axios';
 import moment from 'moment';
 
 import Auth from '../../lib/Auth';
+import { FormGroup, FormControl, Form, Col, Row, ControlLabel, Button } from 'react-bootstrap';
 
 class UserShow extends React.Component {
   state = {
@@ -17,7 +18,6 @@ class UserShow extends React.Component {
       .catch(err => console.error(err));
   }
 
-
   deleteUser = () => {
     Axios
       .delete(`/api/users/${this.props.match.params.id}`, {
@@ -28,83 +28,108 @@ class UserShow extends React.Component {
   }
 
   render(){
-    console.log(this.state.user);
     return(
-      <div className="row">
-        <div className="col-lg-4">
+      <Row>
+        <Col>
           <img src={this.state.user.image} className="img-responsive" />
+        </Col>
+        <Col>
           <h4>{this.state.user.username}</h4>
-          <h4><em>{this.state.user.firstName}</em></h4>
-          <h4><em>{this.state.user.lastName}</em></h4>
-          <h4><em>{this.state.user.email}</em></h4>
+          <h4>{this.state.user.firstName}</h4>
+          <h4>{this.state.user.lastName}</h4>
+          <h4>{this.state.user.email}</h4>
+        </Col>
+        <Col>
           { Auth.isAuthenticated &&
-            <Link
-              to={`/users/${this.state.user.id}/edit`}
-              className="main-button"
-            >
-              <button>Edit</button>
+            <Link to={`/users/${this.state.user.id}/edit`}>
+              <Button>Edit</Button>
             </Link>
           }
           {' '}
           { Auth.isAuthenticated &&
-            <button
-              className="main-button"
-              onClick={this.deleteUser}
-            >
+            <Button onClick={this.deleteUser}>
               Delete
-            </button>
+            </Button>
           }
-        </div>
-        <hr />
-        <div className="col-lg-4">
-          <h5>UPCOMING TRIPS</h5>
-          <Link to="/trips/new" className="main-button">
-            <button>Add Trip</button>
-          </Link>
-          {this.state.user.upcomingTrips && <div>
-            { this.state.user.upcomingTrips.map(trip => {
-              return(
-                <div key={trip.id} className="">
-                  <Link to={`/trips/${trip.id}`}>
-                    <p>{trip.country}</p>
-                    <p>{trip.resort}</p>
-                    <p>{moment(trip.date).format('ddd Do YYYY')}</p>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-          }
-        </div>
-        <div className="col-lg-4">
-          <h5>PAST TRIPS</h5>
-          {this.state.user.pastTrips && <div>
-            { this.state.user.pastTrips.map(trip => {
-              return(
-                <div key={trip.id} className="">
-                  <Link to={`/trips/${trip.id}`}>
-                    <p>{trip.country}</p>
-                    <p>{trip.resort}</p>
-                    <p>{moment(trip.date).format('ddd Do YYYY')}</p>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-          }
-        </div>
-        <hr />
-        <div className="col-lg-4">
-          <h5>DOCUMENTS</h5>
-          <p>add file uploader</p>
-        </div>
-        <hr />
-        <div className="col-lg-4">
-          <h5>MESSAGES</h5>
-          <p>show messages but ideally would like to get an alert of new message and click onto another page to show the private message</p>
-        </div>
-        <hr />
-      </div>
+        </Col>
+
+      </Row>
+      /* <hr /> */
+      // <div className="row">
+        // <div className="col-lg-4">
+          /* <img src={this.state.user.image} className="img-responsive" /> */
+          // <h4>{this.state.user.username}</h4>
+          // <h4><em>{this.state.user.firstName}</em></h4>
+          // <h4><em>{this.state.user.lastName}</em></h4>
+          // <h4><em>{this.state.user.email}</em></h4>
+        //   { Auth.isAuthenticated &&
+        //     <Link
+        //       to={`/users/${this.state.user.id}/edit`}
+        //       className="main-button"
+        //     >
+        //       <button>Edit</button>
+        //     </Link>
+        //   }
+        //   {' '}
+        //   { Auth.isAuthenticated &&
+        //     <button
+        //       className="main-button"
+        //       onClick={this.deleteUser}
+        //     >
+        //       Delete
+        //     </button>
+        //   }
+        // </div>
+
+      //   <div className="col-lg-4">
+      //     <h5>UPCOMING TRIPS</h5>
+      //     <Link to="/trips/new" className="main-button">
+      //       <button>Add Trip</button>
+      //     </Link>
+      //     {this.state.user.upcomingTrips && <div>
+      //       { this.state.user.upcomingTrips.map(trip => {
+      //         return(
+      //           <div key={trip.id} className="">
+      //             <Link to={`/trips/${trip.id}`}>
+      //               <p>{trip.country}</p>
+      //               <p>{trip.resort}</p>
+      //               <p>{moment(trip.date).format('ddd Do YYYY')}</p>
+      //             </Link>
+      //           </div>
+      //         );
+      //       })}
+      //     </div>
+      //     }
+      //   </div>
+      //   <div className="col-lg-4">
+      //     <h5>PAST TRIPS</h5>
+      //     {this.state.user.pastTrips && <div>
+      //       { this.state.user.pastTrips.map(trip => {
+      //         return(
+      //           <div key={trip.id} className="">
+      //             <Link to={`/trips/${trip.id}`}>
+      //               <p>{trip.country}</p>
+      //               <p>{trip.resort}</p>
+      //               <p>{moment(trip.date).format('ddd Do YYYY')}</p>
+      //             </Link>
+      //           </div>
+      //         );
+      //       })}
+      //     </div>
+      //     }
+      //   </div>
+      //   <hr />
+      //   <div className="col-lg-4">
+      //     <h5>DOCUMENTS</h5>
+      //     <p>add file uploader</p>
+      //   </div>
+      //   <hr />
+      //   <div className="col-lg-4">
+      //     <h5>MESSAGES</h5>
+      //     <p>show messages but ideally would like to get an alert of new message and click onto another page to show the private message</p>
+      //   </div>
+      //   <hr />
+      // </div>
     );
   }
 }
---
+++ src/components/utility/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Video extends React.Component {
+  state = {
+    // videoURL: 'src/components/utility/movies/videoplayback.mp4'
+  }
+
+
+  render() {
+    return (
+      <video id="background-video" loop autoPlay>
+        <source src={this.state.videoURL} type="video/mp4" />
+        <source src={this.state.videoURL} type="video/ogg" />
+        Your browser does not support the video tag.
+      </video>
+    );
+  }
+}
+
+export default Video;
