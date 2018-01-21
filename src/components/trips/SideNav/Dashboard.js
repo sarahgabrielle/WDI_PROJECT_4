84b01b99a648546cfb38c209ca9c19bc2aedea9d
sarahalpay <sarah.g.alpay@gmail.com>
2018-01-21 23:19:40 +0000

    commented skycons back into dashboard page

--- src/components/trips/SideNav/Dashboard.js
+++ src/components/trips/SideNav/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
 import moment from 'moment';
-// import '../../utility/skycons';
+import '../../utility/skycons';
 // import BackButton from '../../utility/BackButton';
 
 class DashBoard extends React.Component {
